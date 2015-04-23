2f0b999fe8d38b4d19172ba6991d06bbe28ee927
João Helis Bernardo <joaohelis.bernardo@gmail.com>
2015-04-23 06:09:45 -0300

    Changes in the RunCodeContributionMiner class... Added other miner in the main method

--- src/br/ufrn/ppgsc/backhoe/RunCodeContributionMiner.java
+++ src/br/ufrn/ppgsc/backhoe/RunCodeContributionMiner.java
@@ -1,12 +1,16 @@
 package br.ufrn.ppgsc.backhoe;
 
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Properties;
 
 import br.ufrn.ppgsc.backhoe.exceptions.MissingParameterException;
+import br.ufrn.ppgsc.backhoe.miner.CodeComplexityMiner;
 import br.ufrn.ppgsc.backhoe.miner.CodeContributionMiner;
+import br.ufrn.ppgsc.backhoe.miner.Miner;
 import br.ufrn.ppgsc.backhoe.repository.RepositoryFactory;
 import br.ufrn.ppgsc.backhoe.repository.RepositoryType;
 import br.ufrn.ppgsc.backhoe.repository.code.CodeRepository;
@@ -15,27 +19,38 @@ import br.ufrn.ppgsc.backhoe.util.PropertiesUtil;
 public class RunCodeContributionMiner {
 
 	public static void main(String[] args) {
-		Properties svnProperties = PropertiesUtil.getProperties("svn.properties");
+		Properties svnProperties = PropertiesUtil.getProperties("config/svn.properties");
+		
+		java.util.logging.Logger.getLogger("org.hibernate").setLevel(java.util.logging.Level.OFF); 
 		
 		CodeRepository repository = (CodeRepository) RepositoryFactory.createRepository(RepositoryType.SVN);
 		repository.setUsername(svnProperties.getProperty("username"));
 		repository.setPassword(svnProperties.getProperty("password"));
 		repository.setURL(svnProperties.getProperty("url"));
 		
-		Date startDate = new Date(Date.parse("2014-11-03"));
-		Date endDate = new Date(Date.parse("2014-11-03"));
-		ArrayList<String> developers = new ArrayList(Arrays.asList(new String[]{ "a", "b", "c" }));
+		Date startDate = Date.valueOf("2015-02-09");
+		Date endDate = Date.valueOf("2015-02-10");
+		ArrayList<String> developers = new ArrayList(Arrays.asList(new String[]{"fulano", "sicrano", "beltrano"}));
 		ArrayList<String> ignoredPaths = new ArrayList<String>(Arrays.asList(new String[]{ "/trunk/LPS", "/ExemploIntegracaoSIAFI", "/branches" }));
 		
-		CodeContributionMiner miner = new CodeContributionMiner(repository, startDate, endDate, developers, ignoredPaths);
+		List<Miner> miners = new LinkedList<Miner>();
+		
+		miners.add(new CodeContributionMiner(repository, startDate, endDate, developers, ignoredPaths));
+		miners.add(new CodeComplexityMiner(repository, startDate, endDate, developers, ignoredPaths));
+		
 		try {
-			miner.setup();
-			miner.execute();
+			
+			for(Miner miner: miners){
+				miner.setup();
+				miner.execute();
+			}
+			
 		} catch (MissingParameterException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
+		System.exit(0);
+		
 	}
-
 }
